20822606d6b5f6b0a4d8832c2bb0f6e878bf50c8
Im-Beast <franik.mateusz@gmail.com>
2024-02-22 13:00:38 +0100

    chore: add note about why `Permissions` enum is in a separate file

--- src/_shared/permissions.ts
+++ src/_shared/permissions.ts
@@ -1,3 +1,6 @@
+// Permission needs to be imported from here
+// To prevent circular imports and weird shenanigans
+// like `Permission` becoming undefined from happening
 export enum Permission {
   Read,
   Write,
