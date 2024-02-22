4b787577768ec981dd162d329726351d51308eb9
Im-Beast <franik.mateusz@gmail.com>
2024-02-22 22:42:18 +0100

    feat(write_file): add `writeFile` implementation

--- src/_deno/mod.ts
+++ src/_deno/mod.ts
@@ -4,18 +4,29 @@ import type { PermissionDescriptor, PermissionStatus } from "./permissions.ts";
 export * from "./command.ts";
 export * from "./permissions.ts";
 
+interface ReadFileOptions {
+  signal?: AbortSignal;
+}
+
+interface WriteFileOptions {
+  append?: boolean;
+  create?: boolean;
+  createNew?: boolean;
+  mode?: number;
+  signal?: AbortSignal;
+}
+
 interface Deno {
   env: Map<string, string>;
+
   permissions: {
     query(desc: PermissionDescriptor): Promise<PermissionStatus>;
   };
+
   Command: {
     new (command: string | URL, options?: CommandOptions): Command;
   };
-  readFile(
-    path: string | URL,
-    options?: { signal?: AbortSignal },
-  ): Promise<Uint8Array>;
+
   build: {
     os:
       | "darwin"
@@ -28,6 +39,16 @@ interface Deno {
       | "solaris"
       | "illumos";
   };
+
+  readFile(
+    path: string | URL,
+    options?: ReadFileOptions,
+  ): Promise<Uint8Array>;
+  writeFile(
+    path: string | URL,
+    data: Uint8Array,
+    options?: WriteFileOptions,
+  ): Promise<void>;
 }
 
 // deno-lint-ignore no-explicit-any
---
+++ src/write_file.ts
@@ -0,0 +1,75 @@
+import { Runtime, whichRuntime } from "./which.ts";
+
+import { Deno } from "./_deno/mod.ts";
+import { fs } from "./_node/mod.ts";
+import { posixUrlToPath } from "./_shared/path.ts";
+import { posixPathToHierarchicalPath } from "./_shared/path.ts";
+
+// TODO: normalize errors thrown
+
+interface WriteFileOptions {
+  signal?: AbortSignal;
+  mode?: number;
+}
+
+/**
+ Write `data` to given `path`.
+ The file gets created if it doesn't exist.
+ If a file exists, it gets overwritten.
+
+ In browsers File System API is used and files are written to OPFS.
+ Keep in mind that OPFS is only available in secure contexts (HTTPS/localhost).
+ Path given to the browser must be POSIX-alike.
+
+ @throws
+*/
+export async function writeFile(
+  path: string | URL,
+  data: Uint8Array,
+  options: WriteFileOptions = {},
+): Promise<void> {
+  options.mode ??= 0o666;
+
+  switch (whichRuntime()) {
+    case Runtime.Deno:
+      await Deno.writeFile(path, data, options);
+      break;
+    case Runtime.Node: {
+      const fsModule = await fs();
+      await fsModule.writeFile(path, data, options);
+      break;
+    }
+    case Runtime.Browser: {
+      if (navigator.storage === undefined) {
+        throw new Error("FileSystem API is not supported in this environment.");
+      }
+
+      const root = await navigator.storage.getDirectory();
+      if (path instanceof URL) {
+        path = posixUrlToPath(path);
+      } else if (path.startsWith(".")) {
+        path = path.slice(1);
+      }
+
+      const hierarchicalPath = path.split("/");
+
+      let dir = root;
+      for (const segment of hierarchicalPath.slice(0, -1)) {
+        dir = await dir.getDirectoryHandle(segment);
+      }
+
+      const fileHandle = await dir.getFileHandle(path, {
+        create: true,
+      });
+
+      const writable = await fileHandle.createWritable();
+
+      // We need to truncate the bytes in the file first
+      // So that we don't write on top of existing bytes
+      await writable.truncate(0);
+      await writable.write(data);
+      await writable.close();
+      break;
+    }
+  }
+}
